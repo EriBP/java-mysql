4b179042a241b696eba2afa96a62570fc5a421c8
EriBP <ebarojas@uv.mx>
2022-05-18 17:39:40 -0500

    base de datos

--- proyecto_java_mysql/src/main/java/com/sic/App.java
+++ proyecto_java_mysql/src/main/java/com/sic/App.java
@@ -1,13 +1,37 @@
 package com.sic;
 
+import java.sql.*;
 /**
- * Hello world!
+ 
+ * Consultar datos de MYSQL!
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+       // System.out.println( "Hello World!" );
+       //Try-Catch para manejo de errores
+        try {
+            //Llamada a la libreria mysql conector
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            //Nueva conexión                           conexión a localhost |   nombre de bd | ususario mysql | contraseña  
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/datos", "erikabarojas", "Hallie");
+            //Nuevo statement para llamada de datos
+            Statement stmt = con.createStatement();
+            //Creación de Query | llamada a todos los datos de la tabla alumnos
+            ResultSet rs = stmt.executeQuery("select * from alumnos");
+            //Ciclo de todos los elementos obtenidos por el query
+            while (rs.next())
+                //Impresión de los valores
+                System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3)+ " " + rs.getString(4));
+            //Se cierra la conexión 
+            con.close();
+        } catch (Exception e) {
+            //Imprimir errores 
+            System.out.println(e);
+        }
     }
+
 }
+ 
